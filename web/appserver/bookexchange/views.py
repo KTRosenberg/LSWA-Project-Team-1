e781a3def4c63533c2b0c12ac1654f03f23effb4
KTRosenberg <ktr254@nyu.edu>
2016-12-01 17:30:17 -0500

    some pseudocode maybe

--- web/appserver/bookexchange/views.py
+++ web/appserver/bookexchange/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def get_listings_for_isbn(request):
+	isbn_listings = []
+	dict = request.GET
+	
+	if len(dict) == 0:
+		return render(request, 'book_exchange/listing', isbn_listings)
+		
+	book_listings_data = []
+	user_location = None
+	
+	if request.user.is_authenticated() and request.user.id:
+		user_location = User.location # I don't know how to separate auth user and our User...
+		book_listings_data = BookListing.objects.filter(location, dict)
+	else:
+		book_listings_data = BookListing.objects.filter(dict)
+	
+	for book_listing in book_listings_data:
+		isbn_listings.append((book_listing.seller, book_listing.price, book_listing.condition))
+	return render(request, 'book_exchange/listing', isbn_listings)
