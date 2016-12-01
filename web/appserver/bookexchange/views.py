21b2538bbb88db5dae2956bbb4ec43a97d1efc9f
KTRosenberg <ktr254@nyu.edu>
2016-12-01 17:32:54 -0500

    changed dictionary variable name

--- web/appserver/bookexchange/views.py
+++ web/appserver/bookexchange/views.py
@@ -4,9 +4,9 @@ from .models import *
 
 def get_listings_for_isbn(request):
 	isbn_listings = []
-	dict = request.GET
+	arg_dict = request.GET
 	
-	if len(dict) == 0:
+	if len(arg_dict) == 0:
 		return render(request, 'book_exchange/listing', isbn_listings)
 		
 	book_listings_data = []
@@ -14,9 +14,9 @@ def get_listings_for_isbn(request):
 	
 	if request.user.is_authenticated() and request.user.id:
 		user_location = User.location # I don't know how to separate auth user and our User...
-		book_listings_data = BookListing.objects.filter(location, dict)
+		book_listings_data = BookListing.objects.filter(location, arg_dict)
 	else:
-		book_listings_data = BookListing.objects.filter(dict)
+		book_listings_data = BookListing.objects.filter(arg_dict)
 	
 	for book_listing in book_listings_data:
 		isbn_listings.append((book_listing.seller, book_listing.price, book_listing.condition))
