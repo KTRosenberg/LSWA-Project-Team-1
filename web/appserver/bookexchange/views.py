1607917fd485da4cbcd1573a6275c8b500db8e79
KTRosenberg <ktr254@nyu.edu>
2016-12-01 19:02:55 -0500

    trying to implement buyer-seller inquiry email view

--- web/appserver/bookexchange/views.py
+++ web/appserver/bookexchange/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
+import django.core.mail
 
-def get_isbn_listing_view(request):
+def get_listing_for_isbn(request):
 	isbn_listing_info_out = []
 		
 	if len(arg_dict) == 0:
@@ -17,7 +18,7 @@ def get_isbn_listing_view(request):
 		try:
       		user_profile = UserProfile.objects.get(user_id=request.user.id) # ??
     	except UserProfile.DoesNotExist:
-			return HttpResponse("Invalid user profile")	
+			return HttpResponse("User profile does not exist")	
 		valid_dict['location'] = user_profile.location
 		
 	book_listings = BookListing.objects.filter(**valid_filter_dictarg_dict)
@@ -37,3 +38,46 @@ def get_valid_filter_dict(input_dict):
 			valid_filter_dict[valid_arg] = input_dict[valid_arg]
 			
 	return valid_filter_dict
+	
+def send_purchase_request_email(request):
+
+	if request.user.is_authenticated() and request.user.id:
+	
+		try:
+      		inquirer_profile = UserProfile.objects.get(user_id=request.user.id)
+    	except UserProfile.DoesNotExist:
+			return HttpResponse("Inquirer's user profile does not exist")
+			
+		try:
+      		seller_profile = request.GET.get("seller")
+    	except KeyError:
+			return HttpResponse("Seller's user profile does not exist")
+			
+		try:
+      		isbn_13 = request.GET.get("isbn_13") # somehow map the ISBN to the title for the email?
+    	except KeyError:
+			return HttpResponse("ISBN does not exist")
+			
+		try:
+			book_title = BookListing.objects.get(title=isbn_13)
+		except BookListing.DoesNotExist:
+			return HttpResponse("No book title matching ISBN exists")	
+
+		send_mail(
+			"Book sale inquiry", 
+			"%s would like to buy your copy of %s. Please email them at %s.",
+			inquirer_profile.email, # but this is an email field--can I do this?
+			[seller_profile.email],
+			auth_user=request.user
+		)
+
+	else:
+		return HttpResponse("Not authenticated")	
+
+	
+	
+Contact a Book’s Seller
+Parameters: User (buyer), User (seller), ISBN
+Output: None
+Side Effects: Notifies the seller by email that “$BUYER would like to buy your copy of $BOOK. Please email them at $EMAIL.”
+Authentication: Required (as buyer)
