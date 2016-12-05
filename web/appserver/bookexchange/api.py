60e3245b2a00d5746a7f4976f91c163a7729ecd6
Kate Plajer <kp1573@nyu.edu>
2016-12-05 17:05:06 -0500

    api with edition_info done

--- web/appserver/bookexchange/api.py
+++ web/appserver/bookexchange/api.py
@@ -1,8 +1,9 @@
 import requests
 import json
 
-books_api_key = json.loads(open('../appserver/config.json').read())["google_books_key"]
-isbn_api_key = json.loads(open('../appserver/config.json').read())["isbndb_key"]
+config = json.loads(open('../appserver/config.json').read())
+books_api_key = config["google_books_key"]
+isbn_api_key = config["isbndb_key"]
 
 books_api_url = "https://www.googleapis.com/books/v1/volumes"
 isbn_api_url = "http://isbndb.com/api/v2/json/%s/book" % (isbn_api_key)
@@ -21,54 +22,49 @@ def get_book_by_isbn(isbn):
 	isbn_query_string = 'isbn:%s' % isbn
 	params = { 'key' : books_api_key,
 				'q' : isbn_query_string,
+				'printType' : 'books',
 				'fields' : 'items(volumeInfo(authors,industryIdentifiers,title))'
 			 }
 	book_response = requests.get(books_api_url, params=params)
 	book_data = book_response.json()
-	return book_data['items'][0]
+	if 'items' in book_data:
+		add_editions_to_books(book_data['items'])
+		return book_data['items'][0]
+	else:
+		return {}
 
-	# api_url = "http://isbndb.com/api/v2/json/VHFOB4SZ/book/%s" % (isbn)
-	# book_response = requests.get(api_url)
-	# book_data = book_response.json()
-	# return book_data
 
 def get_books_by_author(author):
 	author_query_string = 'inauthor:"%s"' % author
 	params = { 'key' : books_api_key,
 				'q' : author_query_string,
+				'printType' : 'books',
 				'fields' : 'items(volumeInfo(authors,industryIdentifiers,title))'
 			 }
 	book_response = requests.get(books_api_url, params=params)
 	book_data = book_response.json()
 	if 'items' in book_data:
+		add_editions_to_books(book_data['items'])
 		return book_data['items']
 	else:
 		return {}
 
-	# api_url = "http://isbndb.com/api/v2/json/VHFOB4SZ/books"
-	# params = {'q': author, 'i': 'author_name'}
-	# books_response = requests.get(api_url, params = params)
-	# book_data = books_response.json()
-	# return book_data
 
 def get_books_by_title(title):
 	title_query_string = 'intitle:"%s"' % title
 	params = { 'key' : books_api_key,
 				'q' : title_query_string,
+				'printType' : 'books',
 				'fields' : 'items(volumeInfo(authors,industryIdentifiers,title))'
 			 }
 	book_response = requests.get(books_api_url, params=params)
 	book_data = book_response.json()
 	if 'items' in book_data:
+		add_editions_to_books(book_data['items'])
 		return book_data['items']
 	else:
 		return {}
 
-	# api_url = "http://isbndb.com/api/v2/json/VHFOB4SZ/books"
-	# params = {'q': title}
-	# books_response = requests.get(api_url, params = params)
-	# book_data = books_response.json()
-	# return book_data
 
 def get_books_by_author_and_title(author, title):
 	if not author or author.isspace():
@@ -81,39 +77,33 @@ def get_books_by_author_and_title(author, title):
 		query_string = '%s %s' % (author_query_string, title_query_string)
 		params = { 'key' : books_api_key,
 					'q' : query_string,
+					'printType' : 'books',
 					'fields' : 'items(volumeInfo(authors,industryIdentifiers,title))',
 					'maxResults' : '10'
 				 }
 		book_response = requests.get(books_api_url, params=params)
 		book_data = book_response.json()
-		for book in book_data['items']:
-		    print(book)
-		print()
-		print(book_response.text)
-		print()
 
 		if 'items' in book_data:
-			print(len(book_data['items']))
 			add_editions_to_books(book_data['items'])
 			return book_data['items']
 		else:
 			return {}
 
+
 def add_editions_to_books(books):
-	index = 0
-	for book in books:
+	index = len(books) - 1
+	for book in reversed(books):
 		if "industryIdentifiers" not in book['volumeInfo']:
-			print(book)
 			del books[index]
 		else:
 			try:
 				set_edition_info_for_book(book)
 			except NoIsbnException:
-				print("no isbn")
-				print(book)
 				del books[index]
-		index += 1
+		index -= 1
 	return
+
 
 def get_isbn_for_book_json(book):
 	identifiers = book['volumeInfo']['industryIdentifiers']
@@ -121,6 +111,7 @@ def get_isbn_for_book_json(book):
 		if "ISBN" in identifier['type']:
 			return identifier['identifier']
 	raise NoIsbnException("no isbn identifier present")
+
 
 def set_edition_info_for_book(book):
 	isbn = get_isbn_for_book_json(book)
@@ -131,31 +122,34 @@ def set_edition_info_for_book(book):
 		book['volumeInfo']['edition_info'] = 'unknown'
 	return
 
+
 def get_edition_info_for_isbn(isbn):
 	api_url = "%s/%s" % (isbn_api_url, isbn)
 	book_response = requests.get(api_url)
 	book_data = book_response.json()
 	return book_data
 
+
 class NoIsbnException(Exception):
     pass
 
 
-print("author and title")
-for book in get_books_by_author_and_title("David Lay", "Linear Algebra"):
-	print(book)
-	print()
-print()
-print()
-print("title")
-for book in get_books_by_author_and_title("", "Linear Algebra"):
-	print(book)
-	print()
-print()
-print()
-print("author")
-for book in get_books_by_author_and_title("David Lay", ""):
-	print(book)
-	print()
-print()
-print()
+# print("author and title")
+# for book in get_books_by_author_and_title("David Lay", "Linear Algebra"):
+# 	print(book)
+# 	print()
+# print()
+# print()
+# print("title")
+# for book in get_books_by_author_and_title("", "Linear Algebra"):
+# 	print(book)
+# 	print()
+# print()
+# print()
+# print("author")
+# for book in get_books_by_author_and_title("David Lay", ""):
+# 	print(book)
+# 	print()
+# print()
+# print()
+# print(get_book_by_isbn(9781447911234))
